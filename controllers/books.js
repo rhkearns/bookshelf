293e81f39c6fa4b292138dd4ad418181f4d24205
rhkearns <ryankearns217@gmail.com>
2021-11-05 14:33:40 -0400

    add books.show view

--- controllers/books.js
+++ controllers/books.js
@@ -4,6 +4,19 @@ function newBook(req, res) {
   res.render('books/new', {title: 'New Book'})
 }
 
+function create(req, res) {
+  req.body.finishedReading = !!req.body.finishedReading
+  Book.create(req.body)
+  .then(book => {
+    res.redirect(`/books/${book._id}`)
+  })
+  .catch(err => {
+    console.log(err);
+    res.redirect('/books')
+  })
+}
+
 export {
   newBook as new,
+  create,
 }
--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,7 @@ import * as booksCtrl from '../controllers/books.js'
 const router = Router()
 
 router.get('/new', booksCtrl.new)
+router.post('/books', booksCtrl.create)
 
 export {
   router
