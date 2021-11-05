059ca15cb21e8b4894b675b4294c18154f36fee2
rhkearns <ryankearns217@gmail.com>
2021-11-05 12:17:21 -0400

    add shortcut to user and profile models

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema(
+const Schema = mongoose.Schema
+
+const userSchema = new Schema(
   {
     email: String,
     googleId: String,
