53824b31e3c56e3d39757c67330ed0b5faf57be9
rhkearns <ryankearns217@gmail.com>
2021-11-06 17:36:22 -0400

    add owner property to shelf schema

--- models/shelf.js
+++ models/shelf.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 
 const shelfSchema = new Schema({
   shelfName: String,
-  books: [{type: Schema.Types.ObjectId, ref: 'Book'}]
+  books: [{type: Schema.Types.ObjectId, ref: 'Book'}],
+  owner: [{type: Schema.Types.ObjectId, ref: 'Profile'}],
 })
 
 const Shelf = mongoose.model('Shelf', shelfSchema)
