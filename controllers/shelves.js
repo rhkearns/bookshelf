ee3d85ba7adcf01c210202a4eecdab0003d3e0fd
rhkearns <ryankearns217@gmail.com>
2021-11-06 17:55:55 -0400

    add view shelf index functionality

--- controllers/shelves.js
+++ controllers/shelves.js
@@ -1,6 +1,15 @@
 import { Shelf } from '../models/shelf.js'
 
+function index(req, res) {
+  Shelf.find({})
+  .then(shelves => {
+    res.render('shelves/index', {
+      title: `${req.user.profile.name}'s Shelves`,
+      shelves, 
+    })
+  })
+}
 
 export {
-  
+  index,
 }
