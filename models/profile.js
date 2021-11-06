e1f5905633275fb0b44316b09a1777f1bbd82d9b
rhkearns <ryankearns217@gmail.com>
2021-11-06 17:33:01 -0400

    add books property to shelf schema and shelves property to profiles schema

--- models/profile.js
+++ models/profile.js
@@ -6,6 +6,7 @@ const profileSchema = new Schema({
   name: String,
   avatar: String,
   books: [{type: Schema.Types.ObjectId, ref: 'Book'}],
+  shelves: [{type: Schema.Types.ObjectId, ref: 'Shelf'}],
   }, {
   timestamps: true
 })
--- models/shelf.js
+++ models/shelf.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 
 const shelfSchema = new Schema({
   shelfName: String,
+  books: [{type: Schema.Types.ObjectId, ref: 'Book'}]
 })
 
 const Shelf = mongoose.model('Shelf', shelfSchema)
